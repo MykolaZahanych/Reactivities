92b718d0edc5edce83eb56cf15d3c46cff63df65
Mykola Zahanych <mykola.zahanych@accenture.com>
2022-07-28 16:39:14 +0200

    client image upload

--- client-app/src/app/api/agent.ts
+++ client-app/src/app/api/agent.ts
@@ -2,6 +2,7 @@ import axios, { AxiosResponse } from "axios";
 import { toast } from "react-toastify";
 import { history } from "../..";
 import { Activity, ActivityFormValues } from "../models/activity";
+import { Photo, Profile } from "../models/profile";
 import { User, UserFormValues } from "../models/user";
 import { store } from "../stores/store";
 
@@ -87,9 +88,23 @@ const Account = {
     request.post<User>("/account/register", user),
 };
 
+const Profiles = {
+  get: (username: string) => request.get<Profile>(`/profiles/${username}`),
+  uploadPhoto: (file: Blob) => {
+    let formData = new FormData();
+    formData.append('File', file);
+    return axios.post<Photo>('photos', formData, {
+      headers: {'Content-type': 'multipart/form-data'}
+    })
+  },
+  setMainPhoto: (id: string) => request.post(`/photos/${id}/setMain`, {}),
+  deletePhoto: (id: string) => request.del(`/photos/${id}`)
+}
+
 const agent = {
   Activities,
   Account,
+  Profiles
 };
 
 export default agent;
